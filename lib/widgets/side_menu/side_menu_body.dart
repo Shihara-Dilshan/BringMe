82106f0d8e131b044a05c4912988b97ae354f5b7
Shihara Dilshan <shiharadotadilshanid@gmail.com>
2022-01-08 18:05:12 +0530

    side menu fixes

--- lib/widgets/side_menu/side_menu_body.dart
+++ lib/widgets/side_menu/side_menu_body.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 class SideMenuBody extends StatelessWidget {
   const SideMenuBody({Key? key}) : super(key: key);
+  static const items = [1, 1, 11, 1, 1, 1, 1, 11, 1, 1];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("fdfdf"),
-    );
+    return Expanded(
+        child: Container(
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("sdsd"),
+          );
+        },
+      ),
+    ));
   }
 }
